8b8bf23009c1b18a55db737b8fd7dd14b37955e8
Alec Edgington <alec.edgington@quantinuum.com>
2025-04-09 11:33:44 +0100

    Update dependencies.

--- setup.py
+++ setup.py
@@ -55,9 +55,9 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "azure-quantum >= 3.0.0",
-        "pytket >= 2.0.1",
-        "pytket-qir >= 0.21.1",
+        "azure-quantum >= 3.1.0",
+        "pytket >= 2.2.0",
+        "pytket-qir >= 0.22.0",
     ],
     classifiers=[
         "Environment :: Console",
